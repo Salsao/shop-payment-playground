da430f2cd2b13b795aadd7137d7670692a080c41
Eduardo Sára Pereira <eduardosarapereira@gmail.com>
2020-10-18 13:23:50 -0300

    Added axios and api call for sneakers

--- src/scenes/Store/index.js
+++ src/scenes/Store/index.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import { getSneakers } from '../../api/store';
 
 import Header from '../../components/Header';
 
-const Store = () => (
-  <>
-    <Header title="Sneakers" />
-    Store
-  </>
-);
+import * as S from './styles';
+
+const Store = () => {
+  const [sneakers, setSneakers] = useState([]);
+  const fetchSneakers = useCallback(async () => {
+    const { data } = await getSneakers();
+    setSneakers(data?.results);
+  }, []);
+
+  useEffect(() => {
+    const onLoadPage = () => {
+      fetchSneakers();
+    };
+    onLoadPage();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  console.log(sneakers);
+
+  return (
+    <>
+      <Header title="Sneakers" />
+      <S.Container>Store</S.Container>
+    </>
+  );
+};
 
 export default Store;
